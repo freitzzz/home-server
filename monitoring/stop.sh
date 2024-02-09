10b7324b1d5fa9f89c9c225e47c77c564f7024a9
freitzzz <1160907@isep.ipp.pt>
2024-02-09 23:45:10 +0000

    feat(monitoring): stop all monitoring scripts

---
+++ monitoring/stop.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Stops all monitoring scripts
+
+screen_monitor_id='monitoring'
+
+# First check if there is an active monitoring session
+if screen -ls "$screen_monitor_id" > /dev/null != 0;
+then
+    echo "Monitoring scripts don't seem to be running. Perhaps you want to first start them?"
+    echo '> ./start.sh'
+    exit 1
+fi
+
+# Send kill signal to stop monitoring daemon session
+screen -S $screen_monitor_id -X quit
+
+echo 'Stopped all monitoring scripts.'
